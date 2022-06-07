9d9263c897b97beb7f697f739c3a1f3aececab96
Lina El amrani <linelamrani@gmail.com>
2022-06-07 17:54:00 +0200

    Page profil

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,21 +12,21 @@ Friend.destroy_all
 User.destroy_all
 
 user_1 = User.create!(email: 'user_1@gmail.com', password:'lololo', password_confirmation:'lololo')
-file_1 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1654591985/oi5jpyxnwf2oitdzxxyt.jpg")
+file_1 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/v1654591985/oi5jpyxnwf2oitdzxxyt.jpg")
 user_1.photo.attach(io: file_1, filename: 'pdp1', content_type: 'image/jpg')
 
 user_2 =User.create!(email: 'user_2@gmail.com', password:'lululu', password_confirmation:'lululu')
-file_2 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1654594514/sjyfqmkaci9g5te48lsm.jpg")
+file_2 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/v1654594514/sjyfqmkaci9g5te48lsm.jpg")
 user_2.photo.attach(io: file_2, filename: 'pdp2', content_type: 'image/jpg')
 
 
 user_3 =User.create!(email: 'user_3@gmail.com', password:'lalala', password_confirmation:'lalala')
-file_3 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1654595100/akbogbpj8ie8u3qtvuik.jpg")
+file_3 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/v1654595100/akbogbpj8ie8u3qtvuik.jpg")
 user_3.photo.attach(io: file_3, filename: 'pdp3', content_type: 'image/jpg')
 
 
 user_4 =User.create!(email: 'user_4@gmail.com', password:'lilili', password_confirmation:'lilili')
-file_4 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1654595406/cba3ulehgxdzjc2lzfh4.jpg")
+file_4 = URI.open("https://res.cloudinary.com/du6ejgggh/image/upload/v1654595406/cba3ulehgxdzjc2lzfh4.jpg")
 user_4.photo.attach(io: file_4, filename: 'pdp4', content_type: 'image/jpg')
 
 
