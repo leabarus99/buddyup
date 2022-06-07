d04a509545afc4a15be02ee594d3d6fac99574f2
Valeria Pineda <valerializette.pinedacruz@kedgebs.com>
2022-06-07 11:26:01 +0200

    pundit resolve scope

--- app/policies/friend_policy.rb
+++ app/policies/friend_policy.rb
@@ -1,9 +1,11 @@
 class FriendPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+
+    def resolve
+      scope.all
+    end
+
     def create?
       return true
     end
