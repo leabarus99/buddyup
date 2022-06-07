6f22a87d74e0f8dcc7c3648d6bfb5b830b04c189
Lea Barus <lea.barus31@gmail.com>
2022-06-07 22:24:56 +0200

    fix general interface after merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
-  get "home", to: 'pages#home'
+  root to: 'friends#index'
   get "profil", to: 'pages#profil'
-  get "home", to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :friends
   resources :reservations
