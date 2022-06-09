d4390f67bf7405c38ce42a726e83680b52495add
Lea Barus <lea.barus31@gmail.com>
2022-06-09 12:01:38 +0200

    fix CSS, links and backend

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,8 @@
 class Friend < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :photo
+
   geocoded_by :localisation
   after_validation :geocode, if: :will_save_change_to_localisation?
 end
